71d58dae3eb3f0b9fe1bc0a130902072e884945b
Daito Akimura <dakimura@mercari.com>
2022-09-23 15:02:15 +0900

    use sample code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,36 @@
-import asyncio
-import time
-
-import schedule
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-TRADE_DATA1 = []
-TRADE_DATA2 = []
+st.title('Uber pickups in NYC')
 
+DATE_COLUMN = 'date/time'
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
-async def main():
-    def job1():
-        TRADE_DATA1.append(1)
+@st.cache
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
 
-    def job2():
-        TRADE_DATA2.append(1)
+data_load_state = st.text('Loading data...')
+data = load_data(10000)
+data_load_state.text("Done! (using st.cache)")
 
-    schedule.every(5).seconds.do(job1)
-    schedule.every(30).seconds.do(job2)
+if st.checkbox('Show raw data'):
+    st.subheader('Raw data')
+    st.write(data)
 
-    st.markdown("# Trade1")
-    element1 = st.empty()
+st.subheader('Number of pickups by hour')
+hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+st.bar_chart(hist_values)
 
-    st.markdown("# Trade2")
-    element2 = st.empty()
+# Some number in the range 0-23
+hour_to_filter = st.slider('hour', 0, 23, 17)
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
 
-    while True:
-        schedule.run_pending()
-        element1.dataframe(TRADE_DATA1)
-        element2.dataframe(TRADE_DATA2)
-        time.sleep(1)
-
-
-if __name__ == "__main__":
-    try:
-        asyncio.run(main())
-    except KeyboardInterrupt:
-        pass
+st.subheader('Map of all pickups at %s:00' % hour_to_filter)
+st.map(filtered_data)
